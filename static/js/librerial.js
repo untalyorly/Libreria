841a21e6b45a78a0a5ad60acebbd3de274b539cd
Yorly Parraga <yorliparraga@gmail.com>
2020-07-25 20:42:16 -0500

    estasi

--- static/js/librerial.js
+++ static/js/librerial.js
@@ -10,14 +10,15 @@ if(btnDelete) {
     });
   })
 }
-//cerrar mensaje flash en un determinado tiempo 
 
+//cerrar mensaje flash en un determinado tiempo 
 $(document).ready(function(){
   $(".alert").delay(4000).slideUp(200, function() {
     $(this).alert('close');
   });
 });
 
+//limpiamos los inputs para un nuevo registro
 let btnLimpia = document.querySelector('.limpia');
 let inputs = document.querySelectorAll('input');
 
